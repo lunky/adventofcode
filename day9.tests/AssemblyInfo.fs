bdf54ca85913dd2914a2d6cabe4e88cb8a6bca7f
Quinn Wilson <github@midworld.org>
2016-01-06 14:42:56 -0600

    init working

---
+++ day9.tests/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace day9.tests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("day9.tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Great-West Life")>]
+[<assembly: AssemblyProduct("day9.tests")>]
+[<assembly: AssemblyCopyright("Copyright © Great-West Life 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("fe9887b7-7142-40ef-9371-079a515fc985")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ runner/Program.fs
@@ -0,0 +1,74 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open day9
+open Day9
+open System.IO
+open System.Text.RegularExpressions
+
+[<EntryPoint>]
+let main argv = 
+
+    let citysList  = [
+        {city1= "London"; city2= "Dublin"; distance= 464};
+        {city1= "London"; city2= "Belfast"; distance= 518};
+        {city1= "Dublin"; city2= "Belfast"; distance= 141};
+    ]
+
+    shortestDistance citysList |> printfn "%A" 
+
+    let lines = File.ReadAllLines("day9data.txt") |> Array.toList  
+    //let items = parseFile lines
+
+
+    let (|FirstRegexGroup|_|) pattern input =
+       let m = Regex.Match(input,pattern) 
+       if (m.Success) then Some( m.Groups.[1].Value, m.Groups.[2].Value, m.Groups.[2].Value ) else None 
+
+
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+ 
+//    //Example:
+//    let phone = "(555) 555-5555"
+//    match phone with
+//    | Regex @"\(([0-9]{3})\)[-. ]?([0-9]{3})[-. ]?([0-9]{4})" [ area; prefix; suffix ] ->
+//    printfn "Area: %s, Prefix: %s, Suffix: %s" area prefix suffix
+//    | _ -> printfn "Not a phone number"
+
+         // create a function to call the pattern
+    let testRegex str = 
+        match str with
+        | Regex "(.*?) to (.*) = (\d*)" [incity1; incity2; indistance] -> 
+            {city1= incity1; city2= incity2; distance= System.Int32.Parse indistance}
+
+    let linz = lines |> List.collect (fun l -> [testRegex l])
+    shortestDistance linz |> printfn "Shortest %A" 
+//    shortestRoute linz |> printfn "Shortest %A" 
+    longestDistance linz |> printfn "Longest %A" 
+
+    // test
+//    for line in linz do
+//        printfn "%A" line
+//    let ctz = distinctCities citysList 
+//    let permutations = citysList |> distinctCities |> getPerms ctz.Length
+//
+//    let test = citysList |> getPerms ((ctz.Length)-1)   |> printfn "%A" 
+  
+//    let mapped = getSegments permutations |> List.map (fun segment -> (Seq.map ( fun el -> (cityLookup el citysList) ) segment ))
+//
+//    for perm in mapped do
+//        printfn "route = "
+//        for j in perm do
+//            printfn "from %A to %A = %A "  j.city1 j.city2 j.distance
+//
+//    let shortest = Seq.minBy (fun leg -> Seq.sumBy (fun  x ->  x.distance) leg ) mapped
+//    printfn "Shortest distance = %A  %d" shortest (shortest |> Seq.sumBy (fun  x ->  x.distance))
+//
+//    citysList |> distinctCities |> printfn "%A" 
+
+    let ignoring = System.Console.ReadLine()
+
+    
+    0 // return an integer exit code
